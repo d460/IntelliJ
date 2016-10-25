209bf8efac04cdcf503ad1c4fd11b09be8be1a0d
d460 <dagovf@comunidad.unam.mx>
2016-10-24 23:33:48 -0500

    ByteShortLong

---
+++ ByteShortInt/src/com/d460/Main.java
@@ -0,0 +1,23 @@
+package com.d460;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// int has a width of 32
+        int minValue = -2147483648;
+        int maxValue = 2147483647;
+
+        //byte has a width of 8
+
+        byte myByteValue = -128;
+
+        //short has a width of 16
+
+        short myShortValue = 32767;
+
+        //long has a width of 64
+
+        long myLongValue = 9223372036854775807L;
+
+    }
+}
