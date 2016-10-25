81335399cf1cdc87218cca4617a66a39690748fb
d460 <dagovf@comunidad.unam.mx>
2016-10-24 23:18:12 -0500

    Primer IntelliJ

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Dagoberto on 24/10/2016.
+ */
+public class Hello {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World");
+        int var = 5*4;
+        int res = var;
+    }
+}
