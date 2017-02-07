d114a375fd0150eade579b900191e04ae5be3dcc
d460 <dagovf@comunidad.unam.mx>
2017-02-07 00:58:12 -0600

    Fisrt Commit IntelliJ

--- ByteShortInt/src/com/d460/Main.java
+++ ByteShortInt/src/com/d460/Main.java
@@ -1,5 +1,7 @@
 package com.d460;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -18,6 +20,22 @@ public class Main {
         //long has a width of 64
 
         long myLongValue = 9223372036854775807L;
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Libras: ");
+        double libras = entrada.nextDouble();
+
+        double kilo = .45359237*libras;
+
+        System.out.printf("Kilos: %f\n", kilo);
+
+        double under = 3_455_533.543_45;
+
+        System.out.println("num " + under);
+
+        char myChar = '\u265E';
+        System.out.println("Char " + myChar);
 
     }
 }
---
+++ Composition/src/Wall.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Dagoberto on 06/02/2017.
+ */
+public class Wall {
+
+    private String direction;
+
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}
